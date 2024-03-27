d13f459404c333d0522430ea8525e675b876e50b
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-03-27 10:26:59 +0200

    Changes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { response } = require('express')
 
 app.use(cors())
 
+app.use(express.static('dist'))
+
 morgan.token('type', (req) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
